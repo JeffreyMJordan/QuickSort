707c32514f272d5dba726d4c26c026a5633097cc
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-01-04 17:54:02 -0800

    enables sorting functionality for sort2

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class QuickSort
   # Quick sort has average case time complexity O(nlogn), but worst
   # case O(n**2).
@@ -11,19 +12,29 @@ class QuickSort
 
 
   def self.sort2!(array, start = 0, length = array.length, &prc)
-
+    # subarr = array[start...length]
+    if length<=1 
+      return
+    end 
+    prc ||= Proc.new{|x,y| x<=>y}
+    pivot_idx = partition(array, start, length, &prc)
+    
+    sort2!(array, start, array[start...pivot_idx].length, &prc)
+    sort2!(array, pivot_idx+1, array[pivot_idx+1..-1].length, &prc)
+    array
   end
 
   #Have barrier which starts at first element
   #When you find an element less than the pivot, 
   #swap it with the first element to the right of the barrier, 
   #then iterate barrier by 1
-  #After completing this, swap the pivot with the last element left of the barrie
+  #After completing this, swap the pivot with the last element left of the barrier
   def self.partition(array, start, length, &prc)
+    subarr = array[start...start+length]
     prc ||= Proc.new{|x,y| x<=>y}
     pivot_el = array[start]
     barr = start 
-    idx = start 
+    idx = start+1
     while idx<length+start
       el = array[idx]
       if prc.call(pivot_el, el) == 1 
